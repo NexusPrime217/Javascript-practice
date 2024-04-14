b0490d4ac63c0df3cd5c7bd4cb693d8d2967893a
NexusPrime217 <tulekarabhishek@gmail.com>
2024-04-14 17:04:04 +0530

    Datatype converstion completed,14th April 2024

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,43 @@
+let score="33abc"
+let score2=null
+let score3=undefined
+let score4=true
+
+console.log(typeof score)
+console.log(typeof(score2))
+console.log(typeof(score3))
+console.log(typeof(score4))
+
+let valueInNumber=Number(score)
+let valueInNumber2=Number(score2)
+let valueInNumber3=Number(score3)
+let valueInNumber4=Number(score4)
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber)//Will give NaN 
+console.log(valueInNumber2)//Will give 0 
+console.log(valueInNumber3)//Will give NaN
+console.log(valueInNumber4)//Will give 1 for true and 0 for false
+
+/* "33"->33
+    "33abc"->NaN
+    true->1 false->0
+*/
+
+let isLoggedIn="faf"
+let booleanisLoggendIn=Boolean(isLoggedIn)
+console.log(booleanisLoggendIn)
+
+//1->true 0->false
+//""->false
+//"Abhishek"->true
+
+let someNumber=334
+let someString=String(someNumber)
+console.log(someString)
+console.log(typeof someString)
+
+
+
+
+
