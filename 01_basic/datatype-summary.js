a09e69f8e24dbad97ea3d6cc3e222dcdf8de1237
NexusPrime217 <tulekarabhishek@gmail.com>
2024-04-14 17:44:29 +0530

    Datatypes summarised

---
+++ 01_basic/datatype-summary.js
@@ -0,0 +1,40 @@
+//primitive and non primitive
+
+
+//Primitive
+
+// 7 types:String,Number,Boolean,null,undefined,Symbol,BigInt
+const score=100
+const scoreValue=100.3
+
+const isLoggedIn=true
+const outsideTemp=null
+let userEmail;
+
+const id=Symbol('123')
+const anotherId=Symbol('123')
+
+//console.log(id==anotherId);
+
+const bigNumber=34278345125445n //add n in the end
+
+
+//Reference (Non Primitive)
+
+//Array,Objects, functions
+
+const heros=["Shaktiman","Naagraj","doga"]
+
+let myObj={
+    name:"abhishek",
+    age: 22
+}
+
+const myFunction=function(){
+    console.log("Hello world");
+}
+
+//console.log(typeof myFunction) //return function which is object function.
+
+
+
