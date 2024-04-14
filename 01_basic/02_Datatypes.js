ee2562064dd615f55670bc27bc1eeb12ca3994d4
NexusPrime217 <tulekarabhishek@gmail.com>
2024-04-14 13:48:27 +0530

    Datatypes done. 14th April 2024

---
+++ 01_basic/02_Datatypes.js
@@ -0,0 +1,24 @@
+"use strict";//treat all js code as newer version
+//alert("Hello") //We are using nodeJS not browser.
+
+//console.log(3+3); console.log("Abhishek"); //correct yet should not be used as 
+                                           //code readability is not good
+
+let name="Abhishek"
+let age=18
+let isLoggedIn=false
+let state=null
+//Primitive
+//number=> 2^53
+//bigint
+//string=>""
+//boolean=> true/false
+//null=>standalone value
+//undefined=>
+//symbol =>unique
+
+//object
+
+console.log(typeof"Abhishek")
+console.log(typeof null)//interview question: null is a object
+console.log(typeof undefined)
