dde9516d8d5c3f40faca9d1a29968859fb41b61d
NexusPrime217 <tulekarabhishek@gmail.com>
2024-04-14 17:31:21 +0530

    Comparison done, 14th April 2024

---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,15 @@
+// console.log("1">1)
+// console.log("01">=1)
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0); //comparision and equality works differently
+
+// console.log(undefined>0);
+// console.log(undefined==0);
+// console.log(undefined>=0);
+
+//===
+
+// console.log("2"==2) //first converts and then comparision occurs
+// console.log("2"===2) //compares datatype
