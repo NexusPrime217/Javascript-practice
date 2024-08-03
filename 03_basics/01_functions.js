955a5e2d024de5b543cc6a601e2f445e91a9596b
NexusPrime217 <tulekarabhishek@gmail.com>
2024-08-03 21:32:23 +0530

    function with object done

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -26,7 +26,35 @@ function loginUserMessage(username="sam"){//default value
     return `${username} just logged in`
 }
 // console.log(loginUserMessage("abhi"));
-console.log(loginUserMessage()); //undefined just logged in<-----without if statement
+// console.log(loginUserMessage()); //undefined just logged in<-----without if statement
+
+
+//rest operator
+function calculateCartPrice(...num1){ //looks same as spread operator but they differ based on execution
+    return num1
+}
+
+// console.log(calculateCartPrice(300,400,500));
+
+function calculateCartPriceTwo(val1,val2,...num1){
+    return num1
+}
+// console.log(calculateCartPriceTwo(300,400,200,5000,400)); //here 300 and 400 is given to val1 and val2 so num1 will have 200 5000 400
+
+const user={
+    username:'hitesh',
+    price:199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+//handleObject(user)
+// handleObject({
+//     username:"sam",
+//     price:399
+// })
 
 
 
